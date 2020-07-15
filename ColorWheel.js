069933ff7c5a4028a4d293a5d91117369b327444
tranphuong.080 <tranphuong.080@gmail.com>
2020-07-15 18:44:37 +0700

    fiz style for ios

--- ColorWheel.js
+++ ColorWheel.js
@@ -235,7 +235,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   img: {
-    alignSelf: 'center',
+    alignSelf: 'center',resizeMode:'contain'
   },
   circle: {
     position: 'absolute',
